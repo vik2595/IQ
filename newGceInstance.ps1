9c3a61790c2b2e2190f164fe9abb491b1eac87a5
vik2595 <47865384+vik2595@users.noreply.github.com>
2020-01-13 22:08:35 -0500

    Sample Files

---
+++ newGceInstance.ps1
@@ -0,0 +1,6 @@
+﻿$instancename = Read-Host 'Enter GCE Instance Name'
+$GCEProject = Read-Host 'Enter Porject Name'
+$GCEZone = Read-Host 'Enter GCE Zone'
+$GCEScript = Read-Host 'Enter location on script (path for svrInfo.ps1 file)'
+
+gcloud beta compute --project=$GCEProject instances create $instancename --zone=$GCEZone --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=425425884307-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --image=windows-server-2012-r2-dc-v20191210 --image-project=windows-cloud --boot-disk-size=50GB --boot-disk-type=pd-standard --boot-disk-device-name=test2 --reservation-affinity=any
---
+++ svrInfo.ps1
@@ -0,0 +1,18 @@
+﻿$date = get-date
+$name = (Get-WmiObject Win32_Computersystem).name
+$ip = Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address
+$cpu_usage = (Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select Average ).Average
+$total_processes = get-service | Measure-Object -Line
+$running_process = Get-Service | Where-Object {$_.Status -eq "Running"} | Measure-Object -Line
+Write-Host `nCurrent Date:- 
+format-list -InputObject $date
+Write-Host `nDevice Name:-
+format-list -InputObject $name
+Write-Host `nIP Address:-
+format-list -InputObject $ip
+Write-Host `nCPU Usage:-
+format-list -InputObject $cpu_usage
+Write-Host `nTotal installed processes:-
+format-list -InputObject $total_processes
+Write-Host `nTotal running processes:-
+format-list -InputObject $running_process
